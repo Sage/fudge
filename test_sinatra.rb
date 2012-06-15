a135ceb5a58f1f8f9eea6abb32c18a2f5c89b27d
Jonathan Lozinski <jonathan.lozinski@sage.com>
2012-06-15 08:01:17 +0100

    some work on github connectivity

--- test_sinatra.rb
+++ test_sinatra.rb
@@ -6,6 +6,10 @@ require 'octokit'
 TOKEN = '8363b64e9a7df0ec55074799d3a068ee230bc5a8'
 SECRET = 'cea03fc732957182c80d'
 
+#Until I can get octokit talking oauth properly
+TMP_USER = 'youruser'
+TMP_PASS = 'yourpass'
+
 class TestApp < Sinatra::Base
   use Rack::Session::Cookie
   use OmniAuth::Builder do
@@ -13,9 +17,9 @@ class TestApp < Sinatra::Base
   end
 
   BUILDS = [
-      {status: :pass, name: "Sage One Platform", repo: "Sage/sop.git", id: :sop},
-      {status: :warn, count: 3, name: "Soroban", repo: "Sage/soroban.git", id: :soroban, pulls: true},
-      {status: :fail, count: 6, name: "My Sage One", repo: "Sage/mysageone.git", id: :mysageone}
+      {status: :pass, name: "Sage One Platform", repo: "Sage/sop", id: :sop},
+      {status: :warn, count: 3, name: "Soroban", repo: "Sage/soroban", id: :soroban, pulls: true},
+      {status: :fail, count: 6, name: "My Sage One", repo: "Sage/mysageone", id: :mysageone}
     ]
 
   def auth_hash
@@ -24,6 +28,7 @@ class TestApp < Sinatra::Base
 
   get '/auth/github/callback' do
     github = {}
+    puts auth_hash.inspect
     github[:token] = auth_hash[:credentials][:token]
     github[:user] = auth_hash[:uid]
     session[:github] = github
@@ -34,8 +39,10 @@ class TestApp < Sinatra::Base
     ensure_github
     @build = find_build(params['name'])
     auth = session[:github]
-    github = Octokit::Client.new(:login => auth[:user], :oauth_token => auth[:token])
-    @repo = github.repo @build[:repo]
+    @client = Octokit::Client.new(:oauth_token => auth[:token])
+    @client = Octokit::Client.new(:login => TMP_USER, :password => TMP_PAS)
+    @repo = @client.repo @build[:repo]
+    @pulls = @client.pull_requests @build[:repo]
     erb :repo
   end
 
