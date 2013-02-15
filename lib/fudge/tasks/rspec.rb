2be4ea92d69ca0006ec49ad44cf4ac43af77a8d8
Victor D. G. Martins <correio@victormartins.com>
2013-02-15 17:02:46 +0000

    Makes rpsec coverage pass if no tests exist

--- lib/fudge/tasks/rspec.rb
+++ lib/fudge/tasks/rspec.rb
@@ -8,6 +8,17 @@ module Fudge
 
       private
 
+      # Preconditions to check for coverage, that if not met make the test pass
+      # for example, if no tests exist, no need to fail
+      def pre_conditions_regex
+         /(0 examples, 0 failures)/ # no tests exist
+      end
+
+      # Expression to check for coverage
+      def coverage_regex
+        /((\d+\.\d+)%\) covered)/
+      end
+
       def cmd(options={})
         self.arguments = 'spec/' if arguments.blank?
         bundle_cmd("rspec#{tty_options} #{arguments}", options)
@@ -19,14 +30,30 @@ module Fudge
 
       def check_for
         if coverage
-          [/(\d+\.\d+)%\) covered/, method(:coverage_checker)]
+          [Regexp.new(coverage_regex.source + '|' + pre_conditions_regex.source), method(:coverage_checker)]
         else
           super
         end
       end
 
+      # will check if the expected coverage is met, but first will test for pre conditions to pass
       def coverage_checker(matches)
-        matches[1].to_f >= coverage ? true : 'Insufficient Coverage.'
+        matches = matches.to_s
+        if matches.match(pre_conditions_regex)
+          true
+        else
+          test_coverage_threshold(matches)
+        end
+      end
+
+      # checks the matched string from the console output, to see if the number for the
+      # coverage is greater or equal than the expected coverage
+      def test_coverage_threshold(matches)
+        if matches.to_f >= coverage
+          true
+        else
+          'Insufficient Coverage.'
+        end
       end
     end
 
