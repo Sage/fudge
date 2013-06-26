8e44b61e657225c3953107e935dcf7cf745a1932
Victor D. G. Martins <correio@victormartins.com>
2013-06-26 10:38:45 +0100

    Fixed output encoding issues

--- lib/fudge.rb
+++ lib/fudge.rb
@@ -1,6 +1,14 @@
 require 'rainbow'
 require 'active_support/all'
 
+#TODO: I think it will be safe to remove this once we start using Ruby 2.0
+#This will fix errors: invalid byte sequence in US-ASCII (ArgumentError) when UTF-8 chars get
+#in the console output.
+if RUBY_VERSION =~ /1.9/
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
+
 # Fudge implementation
 module Fudge
   autoload :Build, 'fudge/build'
